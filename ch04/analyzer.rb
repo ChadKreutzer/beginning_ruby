7a8e8990da8d18334c5cb95552174e50a1a59d1c
ChadKreutzer <kanthalion@gmail.com>
2016-11-22 02:15:57 -0700

    working on stop words

--- ch04/analyzer.rb
+++ ch04/analyzer.rb
@@ -1,7 +1,18 @@
 lines = File.readlines("text.txt")
-line_count = lines.size
 text = lines.join
+line_count = lines.size
 total_characters = text.length
+total_characters_nospaces = text.gsub(/\s+/, '').length
+word_count = text.split.length
+paragraph_count = text.split(/\r\n\r\n/).length
+sentence_count = text.split(/\.|\?|!/).length
+stopwords = %w{the a by on for of are with just but and to the by I has some in}
 
 puts "#{line_count} lines"
 puts "#{total_characters} characters"
+puts "#{total_characters_nospaces} characters (excluding spaces)"
+puts "#{word_count} words"
+puts "#{paragraph_count} paragraphs"
+puts "#{sentence_count} sentences"
+puts "#{sentence_count / paragraph_count} sentences per paragraph (average)"
+puts "#{word_count / sentence_count} words per sentence (average)"
